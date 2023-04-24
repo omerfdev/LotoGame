a95617cbb4c26d9a50cb611bd38dae8fccc1f366
omeralmali1 <omeralmali1@gmail.com>
2023-04-24 23:35:47 +0300

    Add project files.

---
+++ LotoGame/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Immutable;
+using System.ComponentModel.Design;
+
+Console.WriteLine("LOTO GAME ! PLEASE ENTER THE NUMBER OF COLUMNS");
+
+int[] kolon = new int[6];
+int columns=Convert.ToInt32(Console.ReadLine());
+for (int j = 0; j < columns; j++)
+{
+    for (int i = 0; i < kolon.Length; i++)
+    {
+        Random rnd = new Random();
+        int sayi = rnd.Next(1, 91);
+        int varmı = Array.IndexOf(kolon, sayi);
+        if (varmı == -1)
+        {
+            kolon[i] = sayi;
+        }
+
+        else
+        {
+            i--;
+        }
+
+    }
+    if (kolon.Count()==6)
+    {
+        Array.Sort(kolon);
+        foreach (var item in kolon)
+        {
+            Console.Write(item + " ");
+
+        }
+        Console.WriteLine("");
+
+    }
+    
+    
+}
+
+
+
+Console.ReadLine();
